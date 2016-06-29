1aa1c2506e39691835bd3a3a086d4d20d8ba58f4
Krishna Durai <krishna.durai@outlook.com>
2016-06-29 21:30:00 +0530

    Configurations for Jenkins and Travis

---
+++ jenkins-build-pipeline.groovy
@@ -0,0 +1,17 @@
+echo "Pipeline for N-ary Tree"
+node {
+    stage 'set-up'
+    sh "pwd > pwd.current"
+    env.WORKSPACE = readFile('pwd.current')
+    sh "rm pwd.current"
+    
+    stage 'checkout'  
+    git url: 'git@github.com:krishnadurai/jervis_test.git'
+    
+    stage 'unit test'
+    sh "nosetests"
+    
+    stage 'build'
+    BUILD_SCRIPT = env.WORKSPACE.trim() + "/build.sh"
+    sh "$BUILD_SCRIPT"
+}
